05332d7448549365a2e06e82b3f24a578bd61593
Segundo Bartra <segundobartra@gmail.com>
2019-03-27 23:40:31 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,6 +2,6 @@
 # See README.md for instructions on how to do this
 def fizzbuzz(number)
   divByThree = ((number % 3) == 0)
-  divByFive = ((number % 5) == 0) 
+  divByFive = ((number % 5) == 0)
   return (divByThree ? (divByFive ? "FizzBuzz" : "Fizz") : (divByFive ? "Buzz" : NIL))
 end
